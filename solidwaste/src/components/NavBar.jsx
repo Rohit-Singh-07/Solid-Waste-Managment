7fff6937adcd0ba8e3a65c53dd71aee4194fb378
Rohit-Singh-07 <150457028+Rohit-Singh-07@users.noreply.github.com>
2024-04-25 14:55:22 +0530

    commit

--- solidwaste/src/components/NavBar.jsx
+++ solidwaste/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <div className="flex justify-center items-center w-[100vw] py-[1.5vw]">
-      <div className="bg-zinc-900 sm:w-[40vw] w-[80vw] z-10 flex justify-around items-center h-[6vh] rounded-2xl text-zinc-400 shadow-[8px_8px_40px_3px_#4359CF83] fixed top-5">
+      <div className="bg-zinc-900 sm:w-[40vw] w-[90vw] z-10 flex justify-around items-center h-[6vh] rounded-2xl text-zinc-400 shadow-[8px_8px_40px_3px_#4359CF83] fixed top-5">
       <NavLink
         to="/"
         className={({ isActive }) => (isActive ? "shadow-[2px_2px_7px_3px_#4359CF83] py-[0.2vw] px-[1vw] hover:text-zinc-100" : "hover:text-zinc-100")}
